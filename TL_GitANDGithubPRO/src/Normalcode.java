39da5698b0e31283ed786cc002021c70b34c6d0b
shubham <shubhammudhol19@gmail.com>
2023-02-25 11:28:37 +0530

    add project

---
+++ TL_GitANDGithubPRO/src/Normalcode.java
@@ -0,0 +1,34 @@
+
+public class Normalcode {
+
+	
+		// display primitive and non primitive data types program
+		public static void main(String [] args)
+		{
+		boolean a=true; //primitive data type
+		char b='a';
+		short c=3;
+		int d=4;
+		float e=4.4f; //value of float data type always write f.
+		long f=8L; //value of long write always L.
+		double g=8.0;
+		byte q;
+		String h="shubham";  // write "" only,we store all data on primitive data type start with uppercase letter
+		
+		//data type used decide on which tpye of data we store in variable.
+		
+		System.out.println(a + "-this is boolean value of boolean data type only in true or false and 1 byte memory");
+		System.out.println(b + "-this is caracter value only used single alfabets and char data type 2 byte ");
+		System.out.println(c +  "-this is short data type it is smaller than int and 2byte");
+		System.out.println(d + "   -this is int data type used to store integer value");
+		System.out.println(e + "   -this is float data type used to store desimal data ,take 4byte");
+		System.out.println(f + "   -this is long data type used to store loger integer data, and take 8byte");
+		System.out.println(g + "   -this is double data type used to store  longer floating value, take 8byte ");
+		System.out.println(h + "   -this is string data type used to store all type of data mixing" + "eg.shu23@@ .");
+		System.out.println(c+d);
+		
+
+
+	}
+
+}
